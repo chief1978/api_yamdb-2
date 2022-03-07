0642c21b3a78c288a8be7a802176cc60f1ba9ea0
Anton Stukach <anton.stukach@gmail.com>
2022-03-07 19:04:15 +0300

    Fixed titles url

--- api_yamdb/titles/urls.py
+++ api_yamdb/titles/urls.py
@@ -1,14 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+from .views import CategoryViewSet, GenreViewSet, TitleViewSet
 
 app_name = 'titles'
 
 router = DefaultRouter()
-router.register(r'categories', FollowViewSet)
-router.register(r'genres', GroupViewSet)
-router.register(r'titles', PostViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'genres', GenreViewSet)
+router.register(r'titles', TitleViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
