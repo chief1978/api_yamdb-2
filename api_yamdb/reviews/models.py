e3a41384317ebc0adc9f856d1617fc3d32b9d2fc
Anton Stukach <anton.stukach@gmail.com>
2022-03-14 23:28:45 +0300

    delete some comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -78,15 +78,6 @@ class Title(models.Model):
     def __str__(self):
         return self.name
 
-    #@property
-    #def rating(self):
-    #    if Review.objects.filter(title_id=self.id).count() == 0:
-    #        return None
-    #    rating = Review.objects.filter(title_id=self.id).aggregate(
-    #        models.Avg('score')
-    #    )
-    #    return(rating['score__avg'])
-
 
 class GenreTitle(models.Model):
     title_id = models.ForeignKey(
